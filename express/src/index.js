1f5c54b2727bc48ccecdbd57b88c43dcbddd0727
NickPentacles <rokosabsolut0@gmail.com>
2024-10-05 04:47:57 +0300

    Add initialization for LmManager

--- express/src/index.js
+++ express/src/index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import router from "./router.js";
+import { lmManager } from "./services.js";
 
 const app = express();
 const port = 3000;
+
 app.use(express.json());
 app.use(router);
-
+app.once("started", async () => {
+  await lmManager.init();
+});
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
+  app.emit("started");
 });
