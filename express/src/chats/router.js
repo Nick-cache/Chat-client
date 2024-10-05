918b3c908345f23cbd6a2fcad92eff5e83fc91c1
NickPentacles <rokosabsolut0@gmail.com>
2024-10-05 19:47:32 +0300

    Add send message route

--- express/src/chats/router.js
+++ express/src/chats/router.js
@@ -7,7 +7,24 @@ export const router = express.Router();
 router.post("/create_chat", async (req, res) => {
   const name = req.body.name;
   const ident = req.body.ident;
-  const model = lmManager.loaded_models[ident];
+  let model = await lmManager.loaded_models[ident];
+  if (model === undefined)
+    return res.status(404).json("No model loaded with this identifier");
   const chat = lmManager.createChat(name, model, ident);
   return res.json(chat);
+});
+
+// request body: {messages: [message,message,...]}
+// message: { role: str, content: str }
+router.post("/chat/:uuid", async (req, res) => {
+  const chat_uuid = req.params.uuid;
+  const messages = req.body.messages;
+  const chat = lmManager.chats[chat_uuid];
+  let stream = chat.stream(messages);
+  for await (const data of stream) {
+    console.log(data);
+  }
+  console.log(stream);
+
+  return res.json("ok");
 });
