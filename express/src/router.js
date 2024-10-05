43c5845f83d93c79250e1288727e4e9f6b8a1674
NickPentacles <rokosabsolut0@gmail.com>
2024-10-05 04:48:30 +0300

    Add unload_model route

--- express/src/router.js
+++ express/src/router.js
@@ -1,21 +1,30 @@
 import express from "express";
 import { lmManager } from "./services.js";
+
 const router = express.Router();
 
 router.get("/list_models", async (req, res) => {
-  const models = await lmManager.getListModels();
-  return res.status(200).json(models);
+  return res.status(200).json(lmManager.models);
 });
 
 // request body: { path: str, ident: str }
 router.post("/load_model", async (req, res) => {
   const path = req.body.path;
-  let ident = req.body.ident;
+  const ident = req.body.ident;
   if (lmManager.loaded_models[ident] !== undefined) {
-    return res.json(`Loaded with ident: ${ident}`);
+    return res.json(`Already loaded with ident: ${ident}`);
   }
   await lmManager.load(path, ident);
   return res.json(`Loaded with ident: ${ident}`);
+});
+
+// request body: { ident: str }
+router.post("/unload_model", async (req, res) => {
+  const ident = req.body.ident;
+  if (lmManager.loaded_models[ident] === undefined)
+    return res.json(`Unloaded ${ident}`);
+  await lmManager.unload(ident);
+  return res.json(`Unloaded ${ident}`);
 });
 
 // request body: { name: str, ident: str }
