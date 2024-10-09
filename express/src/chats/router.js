3b9e16ede6e4d78939747ebfebcbe208f5d2b49d
NickPentacles <rokoslabsolut0@gmail.com>
2024-10-10 02:42:02 +0300

    change route path /chat/:uuid/respond add route /chat/:uuid/resume

--- express/src/chats/router.js
+++ express/src/chats/router.js
@@ -3,6 +3,7 @@ import { lmManager } from "../services.js";
 import { chatWebApi } from "./services.js";
 import { Chat } from "./models.js";
 import { v4 as uuidv4 } from "uuid";
+import { chatInputSocketEvents, chatOutputSocketEvents } from "./socket.js";
 
 export const router = express.Router();
 
@@ -24,22 +25,71 @@ router.post("/create_chat", async (req, res) => {
 
 // message: { role: str, content: str }
 // ! Need to add Redis call
-router.post("/chat/:uuid", async (req, res) => {
+router.post("/chat/:uuid/respond", async (req, res) => {
   const uuid = req.params.uuid;
-  const promt = req.body.promt;
   const chat = lmManager.chats[uuid];
-  const promt_date = new Date().toJSON().slice(0, -5);
-
   if (chat === undefined) return res.status(404).json("Wrong uuid");
   const history = await chatWebApi.get_messages(uuid);
+
+  const promt = req.body.promt;
+  const promt_date = new Date().toJSON().slice(0, -1);
+
+  let isStopped = false;
+  req.socket.addListener(chatInputSocketEvents.messageStop, () => {
+    chat.model.stop();
+    isStopped = true;
+    req.socket.emit("close");
+  });
+
   let content;
   let stream = chat.stream([promt, ...history.data]);
   for await (const data of stream) {
-    // There should be another ws stream which throws data to front - end
+    req.socket.emit(chatOutputSocketEvents.chatStream, data);
     content = content + data;
   }
-  const promtBody = chat.toMessageSchema(promt.role, promt.content, promt_date);
-  const predictionBody = chat.toMessageSchema("assistant", content.slice(9));
-  await chatWebApi.add_messages([promtBody, predictionBody]);
-  return res.json({ content: content, stream: stream });
+
+  const promtBody = chat.toMessageSchema(
+    promt.role,
+    promt.content,
+    false,
+    promt_date
+  );
+  const responseBody = chat.toMessageSchema(
+    "assistant",
+    content.slice(9),
+    isStopped
+  );
+
+  await chatWebApi.add_messages([promtBody, responseBody]);
+  return res.status(200).json("OK");
+});
+
+router.post("/chat/:uuid/resume", async (req, res) => {
+  const uuid = req.params.uuid;
+  const chat = lmManager.chats[uuid];
+  if (chat === undefined) return res.status(404).json("Wrong uuid");
+  const history = await chatWebApi.get_messages(uuid);
+
+  let isStopped = false;
+  req.socket.addListener(chatInputSocketEvents.messageStop, () => {
+    chat.model.stop();
+    isStopped = true;
+    req.socket.emit("close");
+  });
+
+  let content;
+  let stream = chat.stream(history.data);
+  for await (const data of stream) {
+    req.socket.emit(chatOutputSocketEvents.chatStream, data);
+    content = content + data;
+  }
+
+  const responseBody = chat.toMessageSchema(
+    "assistant",
+    content.slice(9),
+    isStopped
+  );
+
+  await chatWebApi.add_messages([promtBody, responseBody]);
+  return res.status(200).json("OK");
 });
