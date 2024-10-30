34c318dfd0385b58237411055603cedef6eb11c2
NickPentacles <rokosabsolut0@gmail.com>
2024-10-30 16:40:15 +0300

    refactor chats dal

--- backend/src/chats/dal.py
+++ backend/src/chats/dal.py
@@ -1,42 +1,88 @@
 from pydantic import UUID4
+from datetime import datetime
 
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import delete, select
+from sqlalchemy import select, update, delete
 
-from src.dal import Dal
+from src.database import DatabaseManager
 from src.chats.models import Chat, Message
 
 
-class ChatDal(Dal):
-    model = Chat
-
-
-class MessageDal(Dal):
-    model = Message
+class ChatDal:
+    @classmethod
+    async def create(cls, name: str, tokens: int):
+        async with DatabaseManager.session_factory() as session:
+            chat = Chat(name=name, tokens=tokens)
+            session.add(chat)
+            await session.commit()
+            await session.refresh(chat)
+            return chat
 
     @classmethod
-    async def delete_messages_by_uuids(
-        cls,
-        uuids: list[UUID4],
-        session: AsyncSession,
-    ):
-        await session.execute(delete(Message).filter(Message.uuid.in_(uuids)))
-        await session.commit()
+    async def change_name(cls, uuid: UUID4, name: str):
+        async with DatabaseManager.session_factory() as session:
+            await session.execute(update(Chat), [{"uuid": uuid, "name": name}])
+            await session.commit()
 
     @classmethod
-    async def get_messages(
-        cls,
-        chat_uuid: UUID4,
-        session: AsyncSession,
+    async def get_by_uuid(cls, uuid: UUID4):
+        async with DatabaseManager.session_factory() as session:
+            return (
+                await session.execute(select(Chat).filter(Chat.uuid == uuid))
+            ).scalar_one_or_none()
+
+    @classmethod
+    async def get_all(cls):
+        async with DatabaseManager.session_factory() as session:
+            return (await session.scalars(select(Chat))).unique().all()
+
+    @classmethod
+    async def delete_by_uuid(cls, uuid: UUID4):
+        async with DatabaseManager.session_factory() as session:
+            await session.execute(delete(Chat).filter(Chat.uuid == uuid))
+            await session.commit()
+
+
+class MessageDal:
+    @classmethod
+    async def create(
+        cls, role: str, content: str, date: datetime, chat_uuid: UUID4, stopped: bool
     ):
-        return (
-            (
-                await session.scalars(
-                    select(Message)
-                    .filter(Message.chat_uuid == chat_uuid)
-                    .order_by(Message.date)
-                )
+        async with DatabaseManager.session_factory() as session:
+            message = Message(
+                role=role,
+                content=content,
+                date=date,
+                chat_uuid=chat_uuid,
+                stopped=stopped,
             )
-            .unique()
-            .all()
-        )
+            session.add(message)
+            await session.commit()
+            await session.refresh(message)
+            return message
+
+    @classmethod
+    async def update_message(cls, uuid: UUID4, content: str):
+        async with DatabaseManager.session_factory() as session:
+            await session.execute(update(Message), [{"uuid": uuid, "content": content}])
+            await session.commit()
+
+    @classmethod
+    async def delete_messages_by_uuids(cls, uuids: list[UUID4]):
+        async with DatabaseManager.session_factory() as session:
+            await session.execute(delete(Message).filter(Message.uuid.in_(uuids)))
+            await session.commit()
+
+    @classmethod
+    async def get_messages(cls, chat_uuid: UUID4):
+        async with DatabaseManager.session_factory() as session:
+            return (
+                (
+                    await session.scalars(
+                        select(Message)
+                        .filter(Message.chat_uuid == chat_uuid)
+                        .order_by(Message.date)
+                    )
+                )
+                .unique()
+                .all()
+            )
