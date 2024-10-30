ceeb3c9a032c25a841a2be36f621620c249b3f2a
NickPentacles <rokosabsolut0@gmail.com>
2024-10-30 16:39:19 +0300

    Refactor config

--- backend/src/config.py
+++ backend/src/config.py
@@ -5,40 +5,34 @@ from pytz import timezone as tz
 
 
 class Settings:
-    def __init__(self, *configs) -> None:
-        for config in configs:
-            setattr(self, config.key, config)
+    def __init__(
+        self,
+        app: BaseSettings,
+        redis: BaseSettings,
+        postgres: BaseSettings,
+        timezone: BaseSettings,
+    ) -> None:
+        self.app = app
+        self.redis = redis
+        self.postgres = postgres
+        self.timezone = timezone
 
 
-class SettingsWithKey(BaseSettings):
-    _key: str
-
-    @property
-    def key(self) -> str:
-        return self._key
-
-
-class App(SettingsWithKey):
-    _key = "app"
-
+class App(BaseSettings):
     host: str = Field(alias="BACKEND_HOST")
     port: int = Field(alias="BACKEND_PORT")
     reload: bool = Field(alias="BACKEND_RELOAD")
     workers: int = Field(alias="BACKEND_WORKERS")
 
 
-class Redis(SettingsWithKey):
-    _key = "redis"
-
+class Redis(BaseSettings):
     host: str = Field(alias="REDIS_HOST")
     port: str = Field(alias="REDIS_PORT")
     broker_idx: int = Field(alias="REDIS_BROKER")
     cache_idx: int = Field(alias="REDIS_CACHE")
 
 
-class Postgres(SettingsWithKey):
-    _key = "postgres"
-
+class Postgres(BaseSettings):
     uri: str = Field(alias="POSTGRES_URI")
 
     @property
@@ -50,9 +44,7 @@ class Postgres(SettingsWithKey):
         return f"postgresql+asyncpg://{self.uri}"
 
 
-class Timezone(SettingsWithKey):
-    _key = "timezone"
-
+class Timezone(BaseSettings):
     raw_timezone: str = Field(alias="TIMEZONE")
 
     @property
