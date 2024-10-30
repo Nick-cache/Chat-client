1a4fb71faa51b50f7a1977046f6576cb34ed0978
NickPentacles <rokosabsolut0@gmail.com>
2024-10-30 16:41:11 +0300

    refactor chats router

--- backend/src/chats/router.py
+++ backend/src/chats/router.py
@@ -1,87 +1,69 @@
 from fastapi.routing import APIRouter
 from pydantic import UUID4
 
-from src.database import session_dep
 from src.utils import to_http_exception
 
 from src.chats.service import ChatService
 from src.chats.exceptions import CHAT_EXCEPTIONS
 from src.chats.schemas import (
-    ChatSaveSchema,
-    ChatResponseSchema,
+    ChatCreateSchema,
+    ChatSchema,
     ChatUpdateSchema,
-    MessageResponseSchema,
+    MessageCreateSchema,
+    MessageUpdateSchema,
+    MessageSchema,
     MessagesDeleteSchema,
 )
 
 router = APIRouter(prefix="/chats", tags=["chats"])
 
 
-@router.post(
-    "/create_chat",
-    response_model=ChatResponseSchema,
-)
+@router.post("/create_chat", response_model=ChatSchema)
 @to_http_exception(CHAT_EXCEPTIONS)
-async def create_chat(
-    payload: ChatSaveSchema,
-    session: session_dep,
-):
-    return await ChatService.create(payload.model_dump(), session)
+async def create_chat(payload: ChatCreateSchema):
+    return await ChatService.create(payload.name, payload.tokens)
 
 
-@router.get(
-    "/get_all",
-    response_model=list[ChatResponseSchema],
-)
-async def get_all_chats(session: session_dep):
-    return await ChatService.get_all(session)
+@router.get("/get_all", response_model=list[ChatSchema])
+async def get_all_chats():
+    return await ChatService.get_all()
 
 
-@router.put("/add_messages")
+@router.put("/add_message")
 @to_http_exception(CHAT_EXCEPTIONS)
-async def add_messages_in_chat(
-    payload: list[MessageResponseSchema],
-    session: session_dep,
-):
-    await ChatService.add_messages(payload, session)
+async def add_message(payload: MessageCreateSchema):
+    await ChatService.add_message(
+        payload.role, payload.content, payload.date, payload.chat_uuid, payload.stopped
+    )
 
 
-@router.put("/update_messages")
+@router.put("/update_message")
 @to_http_exception(CHAT_EXCEPTIONS)
-async def update_messages(
-    payload: list[MessageResponseSchema],
-    session: session_dep,
-):
-    await ChatService.update_messages(payload, session)
+async def update_message(payload: MessageUpdateSchema):
+    await ChatService.update_message(payload.uuid, payload.content)
 
 
 @router.put("/delete_messages")
 @to_http_exception(CHAT_EXCEPTIONS)
-async def delete_messages(
-    payload: MessagesDeleteSchema,
-    session: session_dep,
-):
-    await ChatService.delete_messages(payload.uuids, session)
+async def delete_messages(payload: MessagesDeleteSchema):
+    await ChatService.delete_messages(payload.uuids)
 
 
-@router.get("/{uuid}", response_model=ChatResponseSchema)
-async def get_chat(uuid: UUID4, session: session_dep):
-    ident = {"uuid": uuid}
-    return await ChatService.get(ident, session)
+@router.get("/{uuid}", response_model=ChatSchema)
+async def get_chat(uuid: UUID4):
+    return await ChatService.get_by_uuid(uuid)
 
 
-@router.get("/{uuid}/messages", response_model=list[MessageResponseSchema])
-async def get_chat_messages(uuid: UUID4, session: session_dep):
-    return await ChatService.get_messages(uuid, session)
+@router.get("/{uuid}/messages", response_model=list[MessageSchema])
+async def get_chat_messages(uuid: UUID4):
+    return await ChatService.get_messages(uuid)
 
 
 @router.put("/{uuid}")
-async def update_chat(uuid: UUID4, payload: ChatUpdateSchema, session: session_dep):
-    data = [{"uuid": uuid, "name": payload.name}]
-    await ChatService.update(data, session)
+async def change_chat_name(uuid: UUID4, payload: ChatUpdateSchema):
+    await ChatService.change_name(uuid, payload.name)
 
 
 @router.delete("/{uuid}")
-async def delete_chat(uuid: UUID4, session: session_dep):
-    ident = {"uuid": uuid}
-    await ChatService.delete(ident, session)
+async def delete_chat(uuid: UUID4):
+    await ChatService.delete_by_uuid(uuid)
