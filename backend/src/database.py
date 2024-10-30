74255488c93f3732fee088cfe576ac8a9e0813aa
NickPentacles <rokosabsolut0@gmail.com>
2024-10-30 16:39:58 +0300

    remove session dependency

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,8 +1,5 @@
-from fastapi import Depends
-from typing import Annotated
-
 from sqlalchemy.orm import DeclarativeBase, declared_attr
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from src.config import settings
 
@@ -13,9 +10,6 @@ class BaseModel(DeclarativeBase):
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return f"{cls.__name__.lower()}"
-
-    def update(self, **kwargs):
-        self.__dict__.update(kwargs)
 
 
 class DatabaseManager:
@@ -28,17 +22,3 @@ class DatabaseManager:
         autocommit=False,
         expire_on_commit=False,
     )
-
-    @classmethod
-    async def get_session(cls):
-        try:
-            async with cls.session_factory() as session:
-                yield session
-        except:
-            await session.rollback()
-            raise
-        finally:
-            await session.close()
-
-
-session_dep = Annotated[AsyncSession, Depends(DatabaseManager.get_session)]
