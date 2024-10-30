89ede7d9a45cb0a48ed71fd7bdaba83a58053a8c
NickPentacles <rokosabsolut0@gmail.com>
2024-10-31 00:34:33 +0300

    Refactor load model route, add stream route

--- lms/src/llmodels/router.js
+++ lms/src/llmodels/router.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { lmManager } from "./services.js";
+import { lmManager } from "./service.js";
+import { modelsInputSocketEvents, modelsOutputSocketEvents } from "./events.js";
 
 export const router = express.Router();
 
@@ -18,10 +19,27 @@ router.get("/list_loaded_models", async (req, res) => {
 // request body: { path: str, type: str, ident: str, contentLength: num }
 router.post("/load_model", async (req, res) => {
   const path = req.body.path;
-  const type = req.body.type;
   const contentLength = Number(req.body.contentLength);
   let ident = req.body.ident;
-  ident = await lmManager.load(path, type, ident, contentLength, req.socket);
+
+  const controller = new AbortController();
+
+  const onProgress = (progress) => {
+    req.socket.emit(modelsOutputSocketEvents.loadStream, progress);
+  };
+
+  req.socket.addListener(modelsInputSocketEvents.loadStop, () => {
+    controller.abort();
+    req.socket.emit("close");
+  });
+
+  ident = await lmManager.load(
+    path,
+    ident,
+    contentLength,
+    controller,
+    onProgress
+  );
   return res.json(`Loaded with identifier: ${ident}`);
 });
 
@@ -31,4 +49,37 @@ router.post("/unload_model", async (req, res) => {
   const ident = req.body.ident;
   await lmManager.unload(type, ident);
   return res.json(`Unloaded ${ident}`);
+});
+
+// request body: {history: [messages], promt: message}
+router.post("/:ident/stream", async (req, res) => {
+  const ident = req.params.ident;
+  const model = lmManager.loaded_models[ident];
+  if (model === undefined) return res.status(400).json("Wrong identifier");
+
+  const date = new Date().toJSON().slice(0, -1);
+  const history = req.body.history;
+  const promt = req.body.promt;
+
+  const stream = model.respond([...history, promt]);
+
+  let stopped = false;
+  req.socket.addListener(modelsInputSocketEvents.messageStop, async () => {
+    await stream.cancel();
+    stopped = true;
+    req.socket.emit("close");
+  });
+
+  for await (const data of stream) {
+    req.socket.emit(modelsOutputSocketEvents.messageStream, data);
+  }
+
+  const response = {
+    role: "assistant",
+    content: stream.content.slice(9),
+    date: date,
+    stopped: stopped,
+  };
+
+  return res.json(response);
 });
