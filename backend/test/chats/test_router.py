9b3e5c2007cab1a1bed64a339ec83c084fe4fd0e
NickPentacles <rokosabsolut0@gmail.com>
2024-10-31 00:07:47 +0300

    remove unnecessary tests, refactor existing

--- backend/test/chats/test_router.py
+++ backend/test/chats/test_router.py
@@ -4,27 +4,7 @@ from datetime import datetime, timedelta
 
 
 class TestChats:
-    chat_uuid = uuid4().hex
-    messages = [
-        {
-            "uuid": uuid4().hex,
-            "role": "system",
-            "content": "Lorem Ipsum",
-            "date": datetime.now().__str__(),
-            "chat_uuid": chat_uuid,
-            "stopped": False,
-        },
-        {
-            "uuid": uuid4().hex,
-            "role": "user",
-            "content": "Lorem Ipsum sisdqwdfewfwfwqjwefpoiqwefqweifjoqwifjqwofjweijfowjfewojwefjweoifjwofjwofjwfwoo",
-            "date": (datetime.now() + timedelta(minutes=1)).__str__(),
-            "chat_uuid": chat_uuid,
-            "stopped": False,
-        },
-    ]
     chat = {
-        "uuid": chat_uuid,
         "name": "new_chat",
         "tokens": 100,
     }
@@ -34,113 +14,39 @@ class TestChats:
             url="/chats/create_chat/",
             json=self.chat,
         )
-        assert response.status_code == 200
-
-    def test_get_chat(self, test_app: TestClient):
-        response = test_app.get(url=f"/chats/{self.chat_uuid}/")
+        data = response.json()
+        self.chat_uuid = data["uuid"]
         assert response.status_code == 200
 
     def test_get_all_chats(self, test_app: TestClient):
         response = test_app.get(url="/chats/get_all/")
         assert response.status_code == 200
 
-    def test_add_messages_in_chat(self, test_app: TestClient):
-        messages = [
-            {
-                "uuid": uuid4().hex,
-                "role": "user",
-                "content": "New message",
-                "date": datetime.now().__str__(),
-                "chat_uuid": self.chat_uuid,
-                "stopped": False,
-            },
-            {
-                "uuid": uuid4().hex,
-                "role": "user",
-                "content": "New message",
-                "date": (datetime.now() + timedelta(minutes=1)).__str__(),
-                "chat_uuid": self.chat_uuid,
-                "stopped": False,
-            },
-        ]
+    def test_cant_add_message_in_fake_chat(self, test_app: TestClient):
+        message = {
+            "role": "user",
+            "content": "Fake",
+            "date": (datetime.now() + timedelta(minutes=1)).__str__(),
+            "chat_uuid": uuid4().hex,
+            "stopped": False,
+        }
         response = test_app.put(
-            url="/chats/add_messages/",
-            json=messages,
-        )
-        assert response.status_code == 200
-
-    def test_cant_create_chat_with_fake_messages(self, test_app: TestClient):
-        chat = self.chat
-        chat["messages"] = [
-            {
-                "uuid": uuid4().hex,
-                "role": "user",
-                "content": "New Fake",
-                "date": datetime.now().__str__(),
-                "chat_uuid": uuid4().hex,
-                "stopped": False,
-            },
-            {
-                "uuid": uuid4().hex,
-                "role": "user",
-                "content": "New Fake",
-                "date": (datetime.now() + timedelta(minutes=1)).__str__(),
-                "chat_uuid": uuid4().hex,
-                "stopped": False,
-            },
-        ]
-        response = test_app.post(
-            url="/chats/create_chat/",
-            json=chat,
+            url="/chats/add_message/",
+            json=message,
         )
         assert response.status_code == 422
 
-    def test_cant_add_messages_in_fake_chat(self, test_app: TestClient):
-        messages = [
-            {
-                "uuid": uuid4().hex,
-                "role": "user",
-                "content": "Fake",
-                "date": datetime.now().__str__(),
-                "chat_uuid": uuid4().hex,
-                "stopped": False,
-            },
-            {
-                "uuid": uuid4().hex,
-                "role": "user",
-                "content": "Fake",
-                "date": (datetime.now() + timedelta(minutes=1)).__str__(),
-                "chat_uuid": uuid4().hex,
-                "stopped": False,
-            },
-        ]
-        response = test_app.put(
-            url="/chats/add_messages/",
-            json=messages,
-        )
-        assert response.status_code == 422
-
-    def test_cant_update_fake_messages(self, test_app: TestClient):
-        messages = [
-            {
-                "uuid": uuid4().hex,
-                "role": "user",
-                "content": "Fake Updated",
-                "date": datetime.now().__str__(),
-                "chat_uuid": uuid4().hex,
-                "stopped": False,
-            },
-            {
-                "uuid": uuid4().hex,
-                "role": "user",
-                "content": "Fake Updated",
-                "date": (datetime.now() + timedelta(minutes=1)).__str__(),
-                "chat_uuid": uuid4().hex,
-                "stopped": False,
-            },
-        ]
+    def test_cant_update_fake_message(self, test_app: TestClient):
+        message = {
+            "uuid": uuid4().hex,
+            "role": "user",
+            "content": "Fake Updated",
+            "date": (datetime.now() + timedelta(minutes=1)).__str__(),
+            "chat_uuid": uuid4().hex,
+            "stopped": False,
+        }
         response = test_app.put(
             url="/chats/update_messages/",
-            json={"messages": messages},
+            json=message,
         )
         assert response.status_code == 422
