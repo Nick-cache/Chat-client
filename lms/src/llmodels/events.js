1e596f12f64b75a940ce544924db6807df490a92
NickPentacles <rokosabsolut0@gmail.com>
2024-10-31 00:34:51 +0300

    Add socket events

--- lms/src/llmodels/events.js
+++ lms/src/llmodels/events.js
@@ -1,7 +1,9 @@
 export const modelsInputSocketEvents = {
   loadStop: "loadStop",
+  messageStop: "messageStop",
 };
 
 export const modelsOutputSocketEvents = {
   loadStream: "loadStream",
+  messageStream: "messageStream",
 };
