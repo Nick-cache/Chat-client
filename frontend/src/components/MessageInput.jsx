8cf8abec057d3e60001b4316cfa113bcc19cda5e
NickPentacles <rokosabsolut0@gmail.com>
2024-11-15 00:24:45 +0300

    Change message input styling and add Message element

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const MessageInput = ({ inputValue, handleInputChange, handleSubmit }) => {
   return (
-    <form onSubmit={handleSubmit} className="flex items-center justify-center relative">
-      <textarea
-        value={inputValue}
-        onChange={handleInputChange}
-        placeholder="Type a message..."
-        className="flex-1 border rounded-l p-2 mr-2 min-h-[5rem] h-[5rem]" // Fixed height here
-      />
-      <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-r">Send</button>
-    </form>
+      <>
+        <textarea
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter promt here..."
+          className="border-teal-600 border-x-2 rounded bg-transparent h-[45px] w-[50%] text-center text-emerald-100 caret-pink-500 outline-none font-bold text-sm scrollbar-invise m-0 p-0"
+        />
+        <button type="button" className="ml-8 mb-20 font-bold text-5xl cursor-pointer hover:text-amber-300 border-teal-600/90 text-emerald-300/80">{">"}</button>
+      </>
   );
 };
 
