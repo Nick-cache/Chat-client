c595278fa952829202d099161c10513f9c4611c7
NickPentacles <rokosabsolut0@gmail.com>
2024-11-15 00:18:57 +0300

    Add huggingFace service, which is getting model info through web

--- lms/src/service.js
+++ lms/src/service.js
@@ -1,14 +1,44 @@
 import SDK from "@lmstudio/sdk";
+import axios from "axios";
+
 const { LMStudioClient } = SDK;
 
 const lmsHost = process.env.LMS_LOCAL_HOST;
 const lmsPort = process.env.LMS_LOCAL_PORT;
 
-class LmManager {
-  client = new LMStudioClient({
-    baseUrl: `ws://${lmsHost}:${lmsPort}`,
-  });
+class HuggingFace {
   constructor() {
+    this.client = axios.create({
+      baseURL: "https://huggingface.co",
+    });
+  }
+
+  getBaseModelPath = async (path) => {
+    const res = await this.client.get(`/api/models/${path}`);
+    return res.data.cardData.base_model;
+  };
+
+  getModelConfig = async (path) => {
+    const res = await this.client.get(`/${path}/raw/main/config.json`);
+    const data = {
+      contextLength: res.data.max_position_embeddings,
+      GPULayers: res.data.num_hidden_layers,
+    };
+    return data;
+  };
+
+  getModelInfo = async (path) => {
+    const baseModelPath = await this.getBaseModelPath(path);
+    return await this.getModelConfig(baseModelPath);
+  };
+}
+
+class LmManager {
+  constructor() {
+    this.client = new LMStudioClient({
+      baseUrl: `ws://${lmsHost}:${lmsPort}`,
+    });
+    this.huggingFace = new HuggingFace();
     this.models = {}; // { path: Model }
     this.loaded_models = {}; // ident : model
   }
@@ -25,8 +55,26 @@ class LmManager {
 
   _fillDownloadedModels = async () => {
     const models = await this.client.system.listDownloadedModels();
-    for await (const model of models) {
-      this.models[model.path] = model;
+
+    const added = models.map(async (model) => {
+      if (model.type !== "llm") return null;
+      const formatedPath = /\b.*\//g.exec(model.path)[0];
+      let data = { ...model, formatedPath: formatedPath };
+      try {
+        data = {
+          ...data,
+          ...(await this.huggingFace.getModelInfo(formatedPath)),
+        };
+      } catch (error) {
+        if (error.response.status === 401) {
+          return null;
+        }
+      }
+      return data;
+    });
+
+    for await (const model of added) {
+      if (model !== null) this.models[model.path] = model;
     }
   };
 
@@ -35,13 +83,21 @@ class LmManager {
     await this._fillLoadedModels();
   };
 
-  load = async (path, ident, contextLength, controller, onProgress) => {
+  load = async (
+    path,
+    ident,
+    contextLength,
+    GPULayers,
+    controller,
+    onProgress
+  ) => {
     const model = await this.client.llm.load(path, {
       identifier: ident,
       config: {
         contextLength: contextLength,
+        tryMmap: true,
         gpuOffload: {
-          ratio: 1,
+          ratio: GPULayers,
           mainGpu: 1,
           tensorSplit: [1, 0],
         },
