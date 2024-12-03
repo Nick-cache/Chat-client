28b30fefd888dfffa2d153011b97871f2c02ae01
NickPentacles <rokosabsolut0@gmail.com>
2024-12-03 19:12:21 +0300

    add env for redux devtools

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,16 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { chatsSlice } from "./features/chats/chatsSlice";
+import { chatsSlice, messagesSlice } from "./features/chats/chatsSlice";
 import { lmsSlice, chosenModelSlice } from "./features/lms/lmsSlice";
+import { DEV } from "./config";
 
 export default configureStore({
   reducer: {
     [chatsSlice.reducerPath]: chatsSlice.reducer,
+    [messagesSlice.reducerPath]: messagesSlice.reducer,
     [lmsSlice.reducerPath]: lmsSlice.reducer,
     [chosenModelSlice.name]: chosenModelSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(chatsSlice.middleware)
+      .concat(messagesSlice.middleware)
       .concat(lmsSlice.middleware),
-  devTools: true,
+  devTools: DEV,
 });
